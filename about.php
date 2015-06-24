89e1eb632bfac7ba3e1f981203da8fe831f7aac1
Yuraima Estevez <yuraima.estevez@tommiecopper.com>
2015-06-23 20:59:42 -0400

    fixing validation errors

--- about.php
+++ about.php
@@ -2,23 +2,21 @@
 
   <section class="bg-white">
     <div class="wrapper">
-      <article>
-        <h2>Meet The Instructor</h2>
-        <div class="center-align instructor">
-          <div class="showcase">
-            <div class="circle"></div>
-            <h3>Maria</h3>
-            <ul>
-              <li>Linguist</li>
-              <li>Instructor</li>
-              <li>Learner</li>
-            </ul>
-          </div>
-          <p class="center text-block">
-            My name is Maria Isabel, my first language is spanish, worked as a Teacher Assistant/Spanish interpreter for several years. I am a self-taught linguist who learned English after moving to the U.S. Now, I want to be able you learn Spanish. Let me help you open the door to more opportunities by tutoring you in speaking, reading and writing Spanish.
-          </p>
+      <h2>Meet The Instructor</h2>
+      <div class="center-align instructor">
+        <div class="showcase">
+          <div class="circle"></div>
+          <h3>Maria</h3>
+          <ul>
+            <li>Linguist</li>
+            <li>Instructor</li>
+            <li>Learner</li>
+          </ul>
         </div>
-      </article>
+        <p class="center text-block">
+          My name is Maria Isabel, my first language is spanish, worked as a Teacher Assistant/Spanish interpreter for several years. I am a self-taught linguist who learned English after moving to the U.S. Now, I want to be able you learn Spanish. Let me help you open the door to more opportunities by tutoring you in speaking, reading and writing Spanish.
+        </p>
+      </div>
     </div>
   </section>
   
